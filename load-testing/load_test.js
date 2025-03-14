2e9c80a98dcd8d71b3d3b4a7c798bd504e5bf284
rey455 <ahmedbasharsyed455@gmail.com>
2025-03-14 11:18:17 +0530

    new changes made

--- load-testing/load_test.js
+++ load-testing/load_test.js
@@ -3,7 +3,7 @@ import { check, sleep } from 'k6';
 
 export let options = {
   vus: 50,        // 50 virtual users
-  duration: '30s', // Test duration (30 seconds)
+  duration:'30s', // Test duration (30 seconds)
 };
 
 export default function () {
